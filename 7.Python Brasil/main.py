4dcf6a756b01a38a7ce79113217c2b9f5c480d2e
joaoNote <joaolucasxd012@gmail.com>
2020-08-11 18:27:51 -0300

    estudando

--- 7.Python Brasil/main.py
+++ 7.Python Brasil/main.py
@@ -1,11 +1,10 @@
 # Aqui será testada toda a lógica antes de mandar para classe , e tb testar a a classe
-from Cpf import Cpf
+from Cpf_Cnpj import Documento
 from validate_docbr import CPF # Biblioteca que valida alguns documentos brasileiros
 cpf = "01234567890"
 
-objeto_cpf = Cpf(cpf)
+objeto_cpf = Documento.cria_documento(cpf)
 print(objeto_cpf)
-
-cpf2 = CPF()
-
-print(cpf2.validate(cpf))
+cnpj = "35379838000112"
+objeto_cnpj = Documento.cria_documento(cnpj)
+print(objeto_cnpj)
